dd370ae46624fcaac3f89eaa820b4267cf4ead1d
Fiodar Kazhamiaka <fkazhamiaka@gmail.com>
2022-03-24 09:56:47 -0700

    LiFePO4 battery params and 1/60 simulation sampling

--- src/battery.py
+++ src/battery.py
@@ -53,6 +53,9 @@ class Battery2:
     eff_c = 1
     eff_d = 1
 
+    c_lim = 3
+    d_lim = 3
+
     # Maximum charging energy in one time step 
     # is limited by (u * applied power) + v
     upper_lim_u = 0
@@ -63,55 +66,62 @@ class Battery2:
     lower_lim_u = 0
     lower_lim_v = 0
 
+    # NMC: eff_c = 0.98, eff_d = 1.05,
+    #             c_lim = 3, d_lim = 3,
+    #             upper_u = -0.125, upper_v = 1,
+    #             lower_u = 0.05, lower_v = 0
 
     # defaults for lithium NMC cell
     def __init__(self, capacity, current_load=0,
-                 eff_c = 0.98, eff_d = 1.05,
-                 upper_u = -0.125, upper_v = 1,
-                 lower_u = 0.05, lower_v = 0):
+                 eff_c = 0.97, eff_d = 1.04,
+                 c_lim = 3, d_lim = 3,
+                 upper_u = -0.04, upper_v = 1,
+                 lower_u = 0.01, lower_v = 0):
         self.capacity = capacity
         self.current_load = current_load
 
         self.eff_c = eff_c
         self.eff_d = eff_d
+        self.c_lim = c_lim
+        self.d_lim = d_lim
         self.upper_lim_u = upper_u
         self.upper_lim_v = upper_v
         self.lower_lim_u = lower_u
         self.lower_lim_v = lower_v
 
-    def calc_max_charge(self):
+    def calc_max_charge(self, T_u):
 
         # energy content in current (next) time step: b_k (b_{k+1}, which is just b_k + p_k*eff_c)
         # charging power in current time step: p_k
         # b_{k+1} <= u * p_k + v is equivalent to
         # p_k <= (v - b_k) / (eff_c - u)
-        max_charge = min(self.capacity/self.eff_c, 
-                         (self.upper_lim_v*self.capacity - self.current_load)/(self.eff_c - self.upper_lim_u))
+        max_charge = min((self.capacity/self.eff_c) * self.c_lim, 
+                         (self.upper_lim_v*self.capacity - self.current_load)/((self.eff_c*T_u) - self.upper_lim_u))
         return max_charge
 
-    def calc_max_discharge(self):
+    def calc_max_discharge(self, T_u):
 
         # energy content in current (next) time step: b_k (b_{k+1}, which is just b_k - p_k*eff_d)
         # charging power in current time step: p_k
         # b_{k+1} <= u * p_k + v is equivalent to
         # p_k <= (b_k - v) / (u + eff_d)
-        max_discharge = min(self.capacity/self.eff_d, 
-                            (self.current_load - self.lower_lim_v*self.capacity)/(self.lower_lim_u + self.eff_d))
+        max_discharge = min((self.capacity/self.eff_d) * self.d_lim, 
+                            (self.current_load - self.lower_lim_v*self.capacity)/(self.lower_lim_u + (self.eff_d*T_u)))
         return max_discharge
 
 
     # charge the battery based on an hourly load
     # returns the total load after charging with input_load
-    def charge(self, input_load):
-        max_charge = self.calc_max_charge()
-        self.current_load = self.current_load + min(max_charge, input_load) * self.eff_c
+    def charge(self, input_load, T_u):
+        max_charge = self.calc_max_charge(T_u)
+        self.current_load = self.current_load + (min(max_charge, input_load) * self.eff_c * T_u)
         return self.current_load
 
     # returns how much energy is discharged when
-    # output_load is drawn from the battery in an hour
-    def discharge(self, output_load):
-        max_discharge = self.calc_max_discharge()
-        self.current_load = self.current_load - min(max_discharge, output_load) * self.eff_d
+    # output_load is drawn from the battery over T_u hours (default T_u is 1/60)
+    def discharge(self, output_load, T_u):
+        max_discharge = self.calc_max_discharge(T_u)
+        self.current_load = self.current_load - (min(max_discharge, output_load) * self.eff_d * T_u)
         if(max_discharge < output_load): # not enough battery load
             return max_discharge
         return output_load
@@ -141,21 +151,26 @@ class Battery2:
 # return True if battery can meet all demand, False otherwise
 def sim_battery_247(df_ren, df_dc_pow, b):
 
+    points_per_hour = 60
+
     for i in range(df_dc_pow.shape[0]):
         ren_mw = df_ren[i]
         df_dc = df_dc_pow["avg_dc_power_mw"][i]
         net_load = ren_mw - df_dc
 
-        # surplus, charge
-        if net_load > 0:
-            b.charge(net_load)
+        # Apply the net power points_per_hour times
+        for j in range(points_per_hour):
 
-        else:
-            # deficit, discharge
-            actual_discharge = b.discharge(-net_load)
-            # if we couldnt discharge enough, exit
-            if actual_discharge < -net_load:
-                return False
+            # surplus, charge
+            if net_load > 0:
+                b.charge(net_load, 1/points_per_hour)
+
+            else:
+                # deficit, discharge
+                actual_discharge = b.discharge(-net_load, 1/points_per_hour)
+                # if we couldnt discharge enough, exit
+                if actual_discharge < -net_load:
+                    return False
     return True
 
 
@@ -207,7 +222,7 @@ def calculate_247_battery_capacity_b1_sim(df_ren, df_dc_pow, max_bsize):
 # dc operate on renewables 24/7
 def calculate_247_battery_capacity(df_ren, df_dc_pow):
     battery_cap = 0 # return value stored here, capacity needed
-    b = Battery2(0) # start with an empty battery
+    b = Battery(0) # start with an empty battery
 
     for i in range(df_dc_pow.shape[0]):
         ren_mw = df_ren[i]
@@ -228,7 +243,7 @@ def calculate_247_battery_capacity(df_ren, df_dc_pow):
             if b.capacity > 0:
                 b.charge(ren_mw-df_dc)
             elif b.is_full():
-                b = Battery2(0)
+                b = Battery(0)
 
         if b.capacity > 0 and battery_cap != np.nan:
             battery_cap = max(battery_cap, b.capacity)
